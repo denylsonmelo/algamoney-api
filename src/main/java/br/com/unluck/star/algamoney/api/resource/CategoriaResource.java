033094d3e89ea2a97ab50777f77aa46913d07442
Denylson Melo <denylsonmelo@gmail.com>
2017-10-13 17:03:29 -0300

    retornando 404 caso nao exista a categoria

--- src/main/java/br/com/unluck/star/algamoney/api/resource/CategoriaResource.java
+++ src/main/java/br/com/unluck/star/algamoney/api/resource/CategoriaResource.java
@@ -6,15 +6,12 @@ import java.net.URI;
 import java.util.List;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -36,23 +33,24 @@ public class CategoriaResource {
 
     @PostMapping
     //@ResponseStatus(HttpStatus.CREATED)
-    public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response){
+    public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response) {
         Categoria categoriaSalva = repository.save(categoria);
-    
+
         URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}").buildAndExpand(categoriaSalva.getCodigo()).toUri();
         response.setHeader("Location", uri.toASCIIString());
-    
+
         return ResponseEntity.created(uri).body(categoriaSalva);
     }
-    
+
     @GetMapping("/{codigo}")
-    public Categoria buscarPeloCodigo(@PathVariable Long codigo){
-        return repository.findOne(codigo);
+    public ResponseEntity<Categoria> buscarPeloCodigo(@PathVariable Long codigo) {
+        Categoria categoria = repository.findOne(codigo);
+        return categoria != null ? ResponseEntity.ok(categoria) : ResponseEntity.notFound().build();
     }
-    
+
     @GetMapping("/outro")
-    public String outro(){
+    public String outro() {
         return "OK";
     }
-    
+
 }
